0af82bfdc790aa5ca574dea61a2f6d6d0dd1a33f
Oliver Kurnava <55894461+Oliver81594@users.noreply.github.com>
2024-12-13 18:56:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
     console.log(`Scan result: ${decodedText}`, decodedResult);
+    document.getElementById("ticket_data").innerHTML = decodedText;
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner(
